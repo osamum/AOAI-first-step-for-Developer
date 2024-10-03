0e3085ecc7388fe3de4a4d5b79a14864697722d6
Osamu Monoe <osamum@microsoft.com>
2024-10-03 17:13:06 +0900

    Clean up ex03-3

--- samples/lm_3.js
+++ samples/lm_3.js
@@ -2,21 +2,23 @@
 const { AzureOpenAI } = require("openai");
 const dotenv = require("dotenv");
 dotenv.config();
-const myFunctions = require('./funcs');
+const  myFunctions  = require('./funcs');
 const imageGen = require('./imgGen');
 
-const endpoint = process.env["AZURE_OPENAI_ENDPOINT"];
-const apiKey = process.env["AZURE_OPENAI_API_KEY"];
+const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] ;
+const apiKey = process.env["AZURE_OPENAI_API_KEY"] ;
 const apiVersion = "2024-05-01-preview";
-const deployment = "gpt-4o-mini";
+const deployment = "※ここに言語モデルのデプロイメント名を記述してください"; 
 //言語モデルとユーザーの会話を保持するための配列
 var messages = [
     { role: "system", content: "You are an useful assistant." },
 ];
+//会話を保持する配列の限度
+const messagesLength = 10;
 
 //Azure OpenAI にメッセージを送信する関数
 async function sendMessage(message) {
-    if (message) messages.push({ role: 'user', content: message });
+    if(message) addMessage({ role: 'user', content: message });
     const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });
     const result = await client.chat.completions.create({
         messages: messages,
@@ -27,7 +29,7 @@ async function sendMessage(message) {
     for (const choice of result.choices) {
         if (choice.message.tool_calls) {
             return sendFunctionResult(choice.message);
-        } else {
+        }else{
             const resposeMessage = choice.message.content;
             addMessage({ role: 'assistant', content: resposeMessage });
             return resposeMessage;
@@ -35,7 +37,13 @@ async function sendMessage(message) {
     }
 }
 
-module.exports = { sendMessage };
+//保持する会話の長さを調整する関数
+function addMessage(message) {
+    if(messages.length >= messagesLength) messages.splice(1,1);
+    messages.push(message);
+}
+
+module.exports = {sendMessage};
 
 // tools スキーマの設定
 const tools = [
@@ -46,12 +54,12 @@ const tools = [
             description: 'GitHub アカウントの情報を返す',
             parameters: {
                 type: 'object',
-                properties: {
-                    userName: {
-                        type: 'string',
-                        description: 'GitHub のユーザー名、アカウント名、もしくは ID',
-                    }
-                },
+            properties: {
+                userName: {
+                    type: 'string',
+                    description: 'GitHub のユーザー名、アカウント名、もしくは ID',
+                }
+            },
                 required: ['userName'],
             },
         },
@@ -90,28 +98,28 @@ const tools = [
 async function routingFunctions(name, args) {
     switch (name) {
         case "get_GitHubUser_info":
-            console.log('get_GitHubUser_info');
             return JSON.stringify(await myFunctions.getGitHubUserinfo(args.userName));
         case "get_current_date_time":
             return await myFunctions.getCurrentDatetime();
-        case "generate_image":
-            console.log("\nAI : 画像を生成しています。この処理には数秒かかる場合があります。");
-            return await imageGen.ganarateImage(args.prompt);
+            case "generate_image":
+                console.log("\nAI : 画像を生成しています。この処理には数秒かかる場合があります。");
+                return await imageGen.ganarateImage(args.prompt);
         default:
-            return '要求を満たす関数がありませんでした。';
+        
+        return '要求を満たす関数がありませんでした。';
     }
 }
 
 //アプリケーション内で実行した関数の結果を言語モデルに返す
-async function sendFunctionResult(returnMessage) {
+async function sendFunctionResult(returnMessage){
     const toolCall = returnMessage.tool_calls[0];
     const args = JSON.parse(toolCall.function.arguments);
     const functionResponse = await routingFunctions(toolCall.function.name, args);
 
-    messages.push({
-        role: "function",
-        name: toolCall.function.name,
-        content: functionResponse,
+    addMessage({
+      role: "function",
+      name: toolCall.function.name,
+      content: functionResponse,
     });
     return await sendMessage();
 }
